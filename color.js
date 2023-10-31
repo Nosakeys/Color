b8f77c0b822c30558b6f8a5eeaccd4dcc0141600
Aigbe Silas <93807058+Nosakeys@users.noreply.github.com>
2023-10-31 19:31:06 +0100

    Update color.js

--- color.js
+++ color.js
@@ -2,7 +2,7 @@ const background = document.getElementById('background');
 const btn =document.querySelector("#color");
 const main=document.querySelector('.main');
 
-colorArr = [1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F'];
+colorArr = [1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F','G','H'];
 btn.addEventListener("click", function () {
 
     hexagonal ="#";
